23bf8bd6394422dd4cd62ca74ebc578ec2588f11
Edwin Leonardo Quintero Castaño <equinteroc@choucairtesting.com>
2022-05-03 09:49:30 -0500

    problema con el runner

--- src/test/java/co/com/choucair/certification/mystore/runners/RunnerTags.java
+++ src/test/java/co/com/choucair/certification/mystore/runners/RunnerTags.java
@@ -1,4 +1,16 @@
 package co.com.choucair.certification.mystore.runners;
 
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/driver/chromedriver.exe",
+        tags = "@HistoriaDeUsuario",
+        glue = "co/com/choucair/certification/mystore/stepdefinitions",
+        snippets = CucumberOptions.SnippetType.CAMELCASE)
+
 public class RunnerTags {
 }
+
